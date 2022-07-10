c77222c5df7ec15ae2ac154765c11bb6e5e90be7
kiran <kiran.b@lowes.com>
2022-07-10 22:07:13 +0530

    space removed

--- src/pages/blog/Blog.jsx
+++ src/pages/blog/Blog.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Blog = () => {
-  return <div>For blog page checkout mongo db blog ui that looks good</div>;
+  return <div>For blog page checkout mongodb blog ui that looks good</div>;
 };
 
 export default Blog;
